1075ee8b968a4caf68f53f60f029979e2f3b4702
adam <sonyasania@Laptop-Andini.local>
2026-09-25 16:24:06 +0700

    page baru

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Selamat Datang di Project Erpeel</h1>
+      <p>Ini adalah halaman awal (Home) dari sisi frontend.</p>
+    </div>
+  );
+}
+
+export default Home;
